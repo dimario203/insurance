2221e0c6619eb07aa9d7dae03b041e636ffe85be
Dima <dimario203@gmail.com>
2017-11-27 14:50:07 +0200

    EPL-33 Add new logo

--- frontend/widgets/views/nav-menu.php
+++ frontend/widgets/views/nav-menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark navbar-expand-smd bg-red py-3">
     <div class="container">
-        <a href="/" class="navbar-brand"><img src="images/logo3.png"></a>
+        <a href="/" class="navbar-brand"><img src="images/logo6.png"></a>
         <div class="navbar-collapse d-none d-xl-block" id="collapsingNavbar3">
             <ul class="navbar-nav justify-content-between nav-menu-header">
                 <?=$items?>
